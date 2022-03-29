c80141a030beefbd09e75077376e17086926a1f1
Samuel Mauch <samuel_mauch@gmx.de>
2022-03-29 09:30:25 +0200

    Renaming of agent_handler

--- BERTA/book_operations.py
+++ BERTA/book_operations.py
@@ -1,12 +1,14 @@
+from numpy import arange
 from agent import AgentHandler
 import datetime
 import pandas as pd
 import logging
+from agent import BookingRulesError, BookingConflictsError
 
-def get_my_bookings(agents: AgentHandler):
+def get_my_bookings(agent_handler: AgentHandler, period_id_to_name=False):
     dfs = []
-    for agent in agents.get():
-        df = agent.get_bookings()
+    for agent in agent_handler.get():
+        df = agent.get_bookings(period_id_to_name=period_id_to_name)
         if not df.empty:
             dfs.append(df)
     if not dfs:
@@ -17,25 +19,25 @@ def get_my_bookings(agents: AgentHandler):
         return all_bookings_df
 
 
-def book(agents: AgentHandler, p_agent_id, area, days_delta, reverse_fill, periods, fav_rooms):
+def book(agent_handler: AgentHandler, p_agent_id=None, area=None, days_delta=None, reverse_fill=None, periods=None, fav_rooms=None, **kwargs):
     now = datetime.datetime.now().date()
     if reverse_fill:
         print("Bookings for the next", days_delta, "days...")
         deltas = reversed(range(0, days_delta + 1))
     else:
         print("Booking for in ", days_delta, "days...")
-        deltas = list(days_delta)
+        deltas = [days_delta]
 
     for day_delta in deltas:
         delta = datetime.timedelta(days=day_delta)
         future_date = now + delta
-        df = get_my_bookings(agents)
+        df = get_my_bookings(agent_handler)
         if df.empty:
             booked_periods = []
         else:
             df = df[df['date'] == pd.to_datetime(future_date)]
             booked_periods = df['period'].tolist()
-        free_rooms, room_dict = agents.get(p_agent_id).find_free_place(
+        free_rooms, room_dict = agent_handler.get(p_agent_id).find_free_place(
             future_date, area)
         for period in periods:
             if period not in booked_periods:
@@ -47,34 +49,34 @@ def book(agents: AgentHandler, p_agent_id, area, days_delta, reverse_fill, perio
                 if not p_rooms:
                     logging.warning("No place left")
                     continue
-                for agent in agents.get():
-                    room_id = room_dict[p_rooms[int(len(p_rooms) / 2)]]
+                room_id = room_dict[p_rooms[int(len(p_rooms) / 2)]]
+                for agent in agent_handler.get():
                     booked = agent.book_entry(area=area, room_id=room_id, period=period, date=future_date)
                     if booked:
                         break
                 if not booked:
                     logging.warning("Booking failed for all agents")
-    df = get_my_bookings(agents)
+    df = get_my_bookings(agent_handler)
     print("Bookings:", df)
     return
 
-def delete_all_bookings(agents: AgentHandler):
-    df = get_my_bookings(agents)
+def delete_all_bookings(agent_handler: AgentHandler):
+    df = get_my_bookings(agent_handler)
     if df.empty:
         logging.warning("No rooms to delete")
         return False
     print("Delete bookings...")
     for entry_id, row in df.iterrows():
         _agent_id = str(row['agent'])
-        agents.get(_agent_id).del_entry(entry_id)
-    df = get_my_bookings(agents)
+        agent_handler.get(_agent_id).del_entry(entry_id)
+    df = get_my_bookings(agent_handler)
     print("Bookings:", df)
     return
 
-def change_booking_order(agents: AgentHandler, p_agent_id, area, days_delta):
-    if len(agents) < 2:
+def change_booking_order(agent_handler: AgentHandler, p_agent_id, area, days_delta):
+    if len(agent_handler) < 2:
         return False
-    df = get_my_bookings(agents)
+    df = get_my_bookings(agent_handler)
     if df.empty:
         logging.warning("No rooms to change")
         return False
@@ -82,10 +84,9 @@ def change_booking_order(agents: AgentHandler, p_agent_id, area, days_delta):
     now = datetime.datetime.now().date()
     delta = datetime.timedelta(days_delta)
     tomorrow = now + delta
-    _, room_dict = agents.get(p_agent_id).find_free_place(tomorrow, area)
+    free_rooms, room_dict = agent_handler.get(p_agent_id).find_free_place(tomorrow, area)
     tomorrow_df = df[df['date'] == pd.to_datetime(tomorrow)]
     for entry_id, row in tomorrow_df.iterrows():
-
         _agent_id = str(row['agent'])
         _room_id = room_dict[row['room']]
         _date = row['date']
@@ -93,18 +94,23 @@ def change_booking_order(agents: AgentHandler, p_agent_id, area, days_delta):
 
         if _agent_id != p_agent_id:
             future_entry_df = df[df["agent"] == int(p_agent_id)]['date']
-            agents.get(_agent_id).del_entry(entry_id)
+            agent_handler.get(_agent_id).del_entry(entry_id)
             if not future_entry_df.empty:
                 future_entry_id = future_entry_df.idxmax()
                 f_date = df.loc[future_entry_id]['date']
                 f_room_id = room_dict[df.loc[future_entry_id]['room']]
                 f_period = df.loc[future_entry_id]['period']
                 if f_date != _date:
-                    agents.get(p_agent_id).del_entry(future_entry_id)
-                    agents.get(_agent_id).book_entry(area=area, room_id=f_room_id, period=f_period, date=f_date)
+                    agent_handler.get(p_agent_id).del_entry(future_entry_id)
+                    try:
+                        agent_handler.get(_agent_id).book_entry(area=area, room_id=f_room_id, period=f_period, date=f_date)
+                    except (BookingRulesError, BookingConflictsError):
+                        book(p_agent_id=p_agent_id, area=area, days_delta=(_date - now),reverse_fill=False, periods=_period)
                 df.drop([future_entry_id, entry_id], inplace=True)
-            agents.get(p_agent_id).book_entry(area=area, room_id=_room_id, period=_period, date=_date)
-    df = get_my_bookings(agents)
+            try:
+                agent_handler.get(p_agent_id).book_entry(area=area, room_id=_room_id, period=_period, date=_date)
+            except (BookingRulesError, BookingConflictsError):
+                book(p_agent_id=p_agent_id, area=area, days_delta=(_date - now),reverse_fill=False, periods=_period)
+        #print(get_my_bookings(agent_handler))
+    df = get_my_bookings(agent_handler)
     print("Bookings:", df)
-            
-            
